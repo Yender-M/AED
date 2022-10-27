60fd505285c633c786b6f0273b3e2822d528677d
DJosuep <josuep.garciar@gmail.com>
2022-10-27 14:25:49 -0600

    Añadida clase Nodo basada en el documento

--- src/main/java/aed/lab2/NodosArbol.java
+++ src/main/java/aed/lab2/NodosArbol.java
@@ -10,42 +10,5 @@ package aed.lab2;
  */
 public class NodosArbol {
     
-    Nodo raiz;
     
-    public NodosArbol(){
-        raiz = null;
-    }
-    
-    public void insertar(int i, boolean derecha, boolean izquierda){
-        Nodo n = new Nodo(i);
-        
-        if(raiz == null){
-            raiz = n;
-        }
-        else{
-            //Tiene que poner algo aquí relacionado al número que le corresponde
-            if(derecha == true){
-                //Quizás algo como nodo.derecha = n;
-            }
-            else if(izquierda == false){
-                //nodo.izquierda = n; //o algo así
-            }
-            else{
-            }
-        }
-    }
-    
-    private class Nodo{
-        public Nodo padre;
-        public Nodo derecha;
-        public Nodo izquierda;
-        public int llave;
-        
-        public Nodo(int indice){
-            llave = indice;
-            padre = null;
-            derecha = null;
-            izquierda = null;
-        }
-    }
 }
