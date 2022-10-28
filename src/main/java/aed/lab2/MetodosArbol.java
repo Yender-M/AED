ac42ddcad05a97b941882b18222c1398e48a7e02
DJosuep <josuep.garciar@gmail.com>
2022-10-27 18:45:31 -0600

    Comentariado MedotodosArbol

--- src/main/java/aed/lab2/MetodosArbol.java
+++ src/main/java/aed/lab2/MetodosArbol.java
@@ -8,7 +8,9 @@ package aed.lab2;
  *
  * @author pavel
  */
-public class MetodosArbol {
+public class MetodosArbol extends CrearArbol {
+    //De alguna forma conectar CrearArbol con MetodosArbol
+    
     public void preorden(Nodo nodo){
         if(nodo == null){
             //Escribir nodo.dato
@@ -18,10 +20,18 @@ public class MetodosArbol {
     }
     
     public void inorden(Nodo nodo){
-        
+        if(nodo == null){
+            //preorden(nodo.izq);
+            //Escribir nodo.dato
+            //preorden(nodo.der);
+        }
     }
     
     public void posorden(Nodo nodo){
-        
+        if(nodo == null){
+            //preorden(nodo.izq);
+            //preorden(nodo.der);
+            //Escribir nodo.dato
+        }
     }
 }
